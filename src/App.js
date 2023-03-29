bdf0715130278d190f5ed00e74cbe758f4221b6f
Nai <62731284+tobiastka@users.noreply.github.com>
2023-03-29 11:10:08 -0300

    App

--- src/App.js
+++ src/App.js
@@ -1,23 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react';
+import Calendar from './components/Calendar';
+import Header from './components/Header';
+import TaskCreator from './components/TaskCreator';
+import TaskList from './components/TaskList';
+import './stylesheets/App.css';
 
 function App() {
+
+  const [tasks, setTasks] = useState({
+    toDoTasks: [],
+    completedTasks: []
+  })
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header />
+      <div className="app-body">
+        <div className="app-task-container">
+          <div className="app-new-task">
+            <TaskCreator />
+          </div>
+          <div className="app-tasks-list">
+            <TaskList />
+          </div>
+        </div>
+        <div className="app-calendar-container">
+          <div className="calendar-container">
+            <div className="calendar">
+              <Calendar />
+            </div>
+          </div>
+          <div className="proyect-description">
+
+          </div>
+        </div>
+      </div>
+      <div className="app-footer">
+
+      </div>
     </div>
   );
 }
